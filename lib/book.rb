927a11d9b1f93a870c371e5096e21d5bba0886bc
Cody Rickman <cody@creativemindstudios.com>
2018-12-28 18:33:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
-
-
+class Book
+  def initialize(title)
+    @this_books_title = title
+  end
+  def title
+    @this_books_title
+  end
+  def author=(author)
+    @this_books_author = author
+  end
+  def title
+    @this_books_author
+  end
+  def page_count=(pages)
+    @this_books_title = page_count
+  end
+  def title
+    @this_books_page_count
+  end
+end
